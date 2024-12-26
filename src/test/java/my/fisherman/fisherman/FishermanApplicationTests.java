61ca74cf2cb4d8db555daab2aa506cd4a779c398
sanghee0820 <lsh277604@gmail.com>
2024-12-26 18:24:48 +0900

    project init

---
+++ src/test/java/my/fisherman/fisherman/FishermanApplicationTests.java
@@ -0,0 +1,13 @@
+package my.fisherman.fisherman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishermanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
