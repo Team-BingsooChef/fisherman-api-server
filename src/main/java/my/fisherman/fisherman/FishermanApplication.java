1db4837d3cebcf0bcc577e1d9bc766bde1efef76
sanghee0820 <lsh277604@gmail.com>
2024-12-29 15:45:36 +0900

    feat: BaseEntity 추가

--- src/main/java/my/fisherman/fisherman/FishermanApplication.java
+++ src/main/java/my/fisherman/fisherman/FishermanApplication.java
@@ -2,12 +2,14 @@ package my.fisherman.fisherman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FishermanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FishermanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FishermanApplication.class, args);
+    }
 
 }
