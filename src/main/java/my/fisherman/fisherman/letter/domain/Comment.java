756943e696f375e78223c9520a1ada118c147b46
wgwjh05169 <wgwjh05169@gmail.com>
2025-01-10 12:11:15 +0900

    feat: letter 도메인 작성

---
+++ src/main/java/my/fisherman/fisherman/letter/domain/Comment.java
@@ -0,0 +1,28 @@
+package my.fisherman.fisherman.letter.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment {
+
+    @Column(name = "comment_id")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment_content", nullable = false)
+    private String content;
+
+    @Column(name = "comment_created_time", nullable = false)
+    private LocalDateTime createdTime;
+}
