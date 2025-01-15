246b2713eaa11e21b83326df3377572003787eb0
sanghee0820 <lsh277604@gmail.com>
2025-01-15 20:16:32 +0900

    feat: Email 인증 보내기 및 인증 검증 기능 추가

--- src/main/java/my/fisherman/fisherman/auth/application/AuthService.java
+++ src/main/java/my/fisherman/fisherman/auth/application/AuthService.java
@@ -2,6 +2,8 @@ package my.fisherman.fisherman.auth.application;
 
 import lombok.RequiredArgsConstructor;
 import my.fisherman.fisherman.auth.application.util.CodeGenerator;
+import my.fisherman.fisherman.auth.domain.Authentication;
+import my.fisherman.fisherman.auth.repository.AuthenticationRepository;
 import my.fisherman.fisherman.global.util.MailUtil;
 import my.fisherman.fisherman.global.util.ThymeleafUtil;
 import org.springframework.stereotype.Service;
@@ -9,16 +11,32 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    private static final Integer AUTH_CODE_LENGTH = 6;
-    private static final String AUTH_CODE_SUBJECT = "Fisherman 인증 코드";
+    private final Integer AUTH_CODE_LENGTH = 6;
+    private final String AUTH_CODE_SUBJECT = "Fisherman 인증 코드";
 
     private final ThymeleafUtil thymeleafUtil;
     private final MailUtil mailUtil;
+
+    private final AuthenticationRepository authenticationRepository;
 
     public void sendAuthCode(String email) {
         var authCode = CodeGenerator.generateCode(AUTH_CODE_LENGTH);
         var mailContext = thymeleafUtil.createAuthCodeContext(authCode);
 
         mailUtil.sendMail(email, mailContext, AUTH_CODE_SUBJECT);
+
+        var authentication = new Authentication(email, authCode);
+        authenticationRepository.save(email, authentication);
+    }
+
+    public void verifyAuthCode(String email, String authCode) {
+        var authentication = authenticationRepository.find(email)
+                .orElseThrow(() -> new IllegalArgumentException("인증 정보가 존재하지 않습니다."));
+
+        if (authentication.verify(authCode)) {
+            authenticationRepository.save(email, authentication);
+            return;
+        }
+        throw new IllegalArgumentException("인증 코드가 일치하지 않습니다.");
     }
 }
