f096a778b0994ee960eb2b18f62c9b4510bcb862
sanghee0820 <lsh277604@gmail.com>
2025-01-16 15:51:10 +0900

    feat: Security 기본 설정 추가

---
+++ src/main/java/my/fisherman/fisherman/security/config/SecurityServiceConfig.java
@@ -0,0 +1,30 @@
+package my.fisherman.fisherman.security.config;
+
+import my.fisherman.fisherman.security.application.CustomOAuth2UserService;
+import my.fisherman.fisherman.security.application.SecurityUserService;
+import my.fisherman.fisherman.user.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+
+@Configuration
+public class SecurityServiceConfig {
+
+    @Bean
+    public SecurityUserService securityUserService(UserRepository userRepository) {
+        return new SecurityUserService(userRepository);
+    }
+
+    @Bean
+    public CustomOAuth2UserService customOAuth2UserService(
+            SecurityUserService securityUserService,
+            DefaultOAuth2UserService defaultOAuth2UserService
+    ) {
+        return new CustomOAuth2UserService(defaultOAuth2UserService, securityUserService);
+    }
+
+    @Bean
+    public DefaultOAuth2UserService defaultOAuth2UserService() {
+        return new DefaultOAuth2UserService();
+    }
+}
