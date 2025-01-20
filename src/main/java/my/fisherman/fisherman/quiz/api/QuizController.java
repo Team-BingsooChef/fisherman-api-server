a651f6f0346a03d6f2f93cc143f51d1c71fd4b59
wgwjh05169 <wgwjh05169@gmail.com>
2025-01-20 09:19:29 +0900

    refactor: 공통 요청 URI를 Request mapping으로 묶기

--- src/main/java/my/fisherman/fisherman/quiz/api/QuizController.java
+++ src/main/java/my/fisherman/fisherman/quiz/api/QuizController.java
@@ -5,24 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import my.fisherman.fisherman.quiz.api.request.SolveQuizRequest;
 import my.fisherman.fisherman.quiz.api.response.QuizResponse;
 import my.fisherman.fisherman.quiz.api.response.SolveQuizResponse;
 
+@RequestMapping("/smelts/quizzes")
 @RestController
 public class QuizController implements QuizSpecification{
 
     @Override
-    @GetMapping("/smelts/quizzes/{quiz-id}")
+    @GetMapping("/{quiz-id}")
     public ResponseEntity<QuizResponse> getQuiz(@PathVariable(name = "quiz-id") Long quizId) {
         // TODO
         return null;
     }
 
     @Override
-    @PatchMapping("/smelts/quizzes")
+    @PatchMapping
     public ResponseEntity<SolveQuizResponse> solveQuiz(@RequestBody SolveQuizRequest request) {
         // TODO
         return null;
