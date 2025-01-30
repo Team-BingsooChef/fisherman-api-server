f27c95648f9237fe2ad1de6d8e2c20d397f274ab
HyeonJeong <wgwjh05169@gmail.com>
2025-01-30 18:29:45 +0900

    refactor: 빙어 상태 구체화

--- src/main/java/my/fisherman/fisherman/smelt/domain/SmeltStatus.java
+++ src/main/java/my/fisherman/fisherman/smelt/domain/SmeltStatus.java
@@ -1,5 +1,8 @@
 package my.fisherman.fisherman.smelt.domain;
 
 public enum SmeltStatus {
-    SOLVED, READ, UNREAD
+    DREW,       // 뽑음 (다른 낚시터에 보내지 않음)
+    UNREAD,     // 읽지 않음. 퀴즈가 있다면 풀지 않음
+    SOLVED,     // 문제를 풀었으나 읽지 않음
+    READ,       // 읽음
 }
