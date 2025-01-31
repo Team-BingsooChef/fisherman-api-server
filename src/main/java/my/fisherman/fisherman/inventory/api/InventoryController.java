47d57e9b1627c6210550f9ec8884c4ab511eee73
HyeonJeong <wgwjh05169@gmail.com>
2025-02-01 00:05:31 +0900

    feat: InventoryController에 빙어 뽑기 API 구현

--- src/main/java/my/fisherman/fisherman/inventory/api/InventoryController.java
+++ src/main/java/my/fisherman/fisherman/inventory/api/InventoryController.java
@@ -1,18 +1,30 @@
 package my.fisherman.fisherman.inventory.api;
 
+import lombok.RequiredArgsConstructor;
 import my.fisherman.fisherman.inventory.api.response.InventoryResponse;
+import my.fisherman.fisherman.inventory.application.InventoryService;
+import my.fisherman.fisherman.inventory.application.dto.InventoryInfo;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/inventories/{inventory-id}/smelts")
 @RestController
 public class InventoryController implements InventorySpecification {
+    private final InventoryService inventoryService;
 
     @Override
     @PostMapping
     public ResponseEntity<InventoryResponse.DrewSmelt> drawSmelt(@PathVariable(name = "inventory-id") Long inventoryId) {
-        // TODO
-        return null;
+        // TODO: 사용자 ID를 인증 정보에서 가져오도록 수정
+        Long userId = 1L;
+
+        InventoryInfo.SmeltInfo info = inventoryService.drawSmelt(userId, inventoryId);
+
+        InventoryResponse.DrewSmelt response = InventoryResponse.DrewSmelt.from(info);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @Override
