d039717c692255ee5e81595a08e627519469d423
HyeonJeong <wgwjh05169@gmail.com>
2025-01-31 22:29:05 +0900

    feat: SmeltTypeRepository 인터페이스 작성

---
+++ src/main/java/my/fisherman/fisherman/smelt/repository/SmeltTypeRepository.java
@@ -0,0 +1,9 @@
+package my.fisherman.fisherman.smelt.repository;
+
+import my.fisherman.fisherman.smelt.domain.SmeltType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SmeltTypeRepository extends JpaRepository<SmeltType, Long> {
+}
