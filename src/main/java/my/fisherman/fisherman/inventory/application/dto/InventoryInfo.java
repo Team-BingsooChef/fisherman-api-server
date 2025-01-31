4c4f90ee9ad4a15f60c4a0084c8ba9876646f371
HyeonJeong <wgwjh05169@gmail.com>
2025-02-01 00:03:23 +0900

    refactor: InventoryInfo에 도메인 정보 제거

--- src/main/java/my/fisherman/fisherman/inventory/application/dto/InventoryInfo.java
+++ src/main/java/my/fisherman/fisherman/inventory/application/dto/InventoryInfo.java
@@ -1,29 +1,23 @@
 package my.fisherman.fisherman.inventory.application.dto;
 
-import my.fisherman.fisherman.fishingspot.domain.FishingSpot;
-import my.fisherman.fisherman.inventory.domain.Inventory;
 import my.fisherman.fisherman.smelt.domain.Smelt;
-import my.fisherman.fisherman.smelt.domain.Letter;
 import my.fisherman.fisherman.smelt.domain.SmeltStatus;
-import my.fisherman.fisherman.smelt.domain.SmeltType;
 
 public class InventoryInfo {
     public record SmeltInfo(
             Long id,
-            Inventory inventory,
-            FishingSpot fishingSpot,
+            Long inventoryId,
+            Long fishingSpotId,
             SmeltStatus status,
-            SmeltType type,
-            Letter letter
+            Long smeltTypeId
     ) {
         public static SmeltInfo from(Smelt smelt) {
             return new SmeltInfo(
                     smelt.getId(),
-                    smelt.getInventory(),
-                    smelt.getFishingSpot(),
+                    smelt.getInventory().getId(),
+                    smelt.getFishingSpot().getId(),
                     smelt.getStatus(),
-                    smelt.getType(),
-                    smelt.getLetter()
+                    smelt.getType().getId()
             );
         }
     }
