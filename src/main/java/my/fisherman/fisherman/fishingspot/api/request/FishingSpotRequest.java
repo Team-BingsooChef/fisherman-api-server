d8aa46e7ffafce3ced372ad3caaff3e6d46f792e
정현정 <wgwjh05169@gmail.com>
2025-02-03 16:41:47 +0900

    feat: 빙어 보내기 API에 퀴즈 추가

--- src/main/java/my/fisherman/fisherman/fishingspot/api/request/FishingSpotRequest.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/api/request/FishingSpotRequest.java
@@ -1,11 +1,22 @@
 package my.fisherman.fisherman.fishingspot.api.request;
 
+import java.util.List;
+
 public class FishingSpotRequest {
 
     public record Send(
             Long smeltId,
             String title,
             String content,
-            String senderName
+            String senderName,
+            Quiz quiz
+    ) {}
+
+    record Quiz(
+        String title,
+        String content,
+        String type,
+        List<String> questions,
+        int answerIndex
     ) {}
 }
