5e78bfe5c18541e73f18cb98ca2292f5f18332b6
sanghee0820 <lsh277604@gmail.com>
2025-02-04 21:01:20 +0900

    feat: FishingSpot 검색 API 추가

--- src/main/java/my/fisherman/fisherman/fishingspot/api/response/FishingSpotResponse.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/api/response/FishingSpotResponse.java
@@ -1,41 +1,65 @@
 package my.fisherman.fisherman.fishingspot.api.response;
 
 import java.util.List;
+import my.fisherman.fisherman.fishingspot.application.dto.FishingSpotInfo;
 
 public class FishingSpotResponse {
 
     public record ReceivedSmelt(
-            DetailSmelt smelt,
-            Letter letter
-    ) {}
+        DetailSmelt smelt,
+        Letter letter
+    ) {
+
+    }
 
     public record Page(
-            String nickname,
-            int currPage,
-            int totalPages,
-            int totalElements,
-            List<SimpleSmelt> smelts
-    ) {}
+        String nickname,
+        int currPage,
+        int totalPages,
+        int totalElements,
+        List<SimpleSmelt> smelts
+    ) {
+
+    }
 
     record SimpleSmelt(
-            Long id,
-            Long smeltTypeId,
-            String status
-    ) {}
+        Long id,
+        Long smeltTypeId,
+        String status
+    ) {
+
+    }
 
     record DetailSmelt(
-            Long id,
-            Long inventoryId,
-            Long fishingSpotId,
-            Long smeltTypeId,
-            String status
-    ) {}
+        Long id,
+        Long inventoryId,
+        Long fishingSpotId,
+        Long smeltTypeId,
+        String status
+    ) {
+
+    }
 
     record Letter(
-            Long id,
-            String senderName,
-            String title,
-            String content,
-            String createdTime
-    ) {}
+        Long id,
+        String senderName,
+        String title,
+        String content,
+        String createdTime
+    ) {
+
+    }
+
+    public record FishingSpot(
+        Long fishingSpotId,
+        String nickname
+    ) {
+
+        public static FishingSpot from(FishingSpotInfo.Simple fishingSpot) {
+            return new FishingSpot(
+                fishingSpot.fishingSpotId(),
+                fishingSpot.nickname()
+            );
+        }
+    }
 }
