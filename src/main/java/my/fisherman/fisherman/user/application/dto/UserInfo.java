630e163519bc8bf26350f1f178a4b988f4c77c41
sanghee0820 <lsh277604@gmail.com>
2025-02-04 12:24:49 +0900

    feat: user정보 검색 기능 추가

---
+++ src/main/java/my/fisherman/fisherman/user/application/dto/UserInfo.java
@@ -0,0 +1,18 @@
+package my.fisherman.fisherman.user.application.dto;
+
+import my.fisherman.fisherman.user.domain.User;
+
+public class UserInfo {
+
+    public record Simple(
+        String email,
+        String nickname,
+        Boolean isPublic
+    ) {
+
+        public static Simple from(User user) {
+            return new Simple(user.getEmail(), user.getNickname(), user.getIsPublic());
+        }
+    }
+
+}
