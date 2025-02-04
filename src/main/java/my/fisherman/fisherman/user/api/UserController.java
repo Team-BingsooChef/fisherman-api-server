cf39bcc26d92ecb22dffdf133207b8b569759f1c
sanghee0820 <lsh277604@gmail.com>
2025-02-04 12:25:29 +0900

    feat: 유저 정보 검색 API 추가

--- src/main/java/my/fisherman/fisherman/user/api/UserController.java
+++ src/main/java/my/fisherman/fisherman/user/api/UserController.java
@@ -4,8 +4,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import my.fisherman.fisherman.user.api.dto.UserRequest;
+import my.fisherman.fisherman.user.api.dto.UserResponse.Info;
 import my.fisherman.fisherman.user.application.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController implements UserSpecification {
+
     private final UserService userService;
 
     @Override
@@ -24,5 +28,14 @@ public class UserController implements UserSpecification {
         var command = request.toCommand();
         userService.signUp(command);
         return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @GetMapping("/{userId}")
+    public ResponseEntity<Info> getMyInfo(@PathVariable Long userId) {
+        var userInfo = userService.getMyInfo(userId);
+        
+        return ResponseEntity.ok(Info.from(userInfo));
+
     }
 }
