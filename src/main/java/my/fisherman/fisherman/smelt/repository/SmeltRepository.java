d8f483cd3bad2e512fa59d3eaaef781da6564ebe
정현정 <wgwjh05169@gmail.com>
2025-02-09 16:03:49 +0900

    feat: SmeltRepository에 Inventory로 검색하는 메소드 작성

--- src/main/java/my/fisherman/fisherman/smelt/repository/SmeltRepository.java
+++ src/main/java/my/fisherman/fisherman/smelt/repository/SmeltRepository.java
@@ -1,9 +1,13 @@
 package my.fisherman.fisherman.smelt.repository;
 
+import my.fisherman.fisherman.inventory.domain.Inventory;
 import my.fisherman.fisherman.smelt.domain.Smelt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SmeltRepository extends JpaRepository<Smelt, Long> {
+    List<Smelt> findAllByInventoryIs(Inventory inventory);
 }
