d9b2011a87cd7c01064aa8e02e7479d5e7caa7a5
정현정 <wgwjh05169@gmail.com>
2025-02-09 16:04:51 +0900

    feat: InventoryInfo에 빙어의 편지와 코멘트 내용을 포함해 반환하는 DTO 추가

--- src/main/java/my/fisherman/fisherman/inventory/application/dto/InventoryInfo.java
+++ src/main/java/my/fisherman/fisherman/inventory/application/dto/InventoryInfo.java
@@ -1,7 +1,11 @@
 package my.fisherman.fisherman.inventory.application.dto;
 
+import my.fisherman.fisherman.smelt.domain.Comment;
+import my.fisherman.fisherman.smelt.domain.Letter;
 import my.fisherman.fisherman.smelt.domain.Smelt;
 import my.fisherman.fisherman.smelt.domain.SmeltStatus;
+
+import java.time.LocalDateTime;
 
 public class InventoryInfo {
     public record SmeltInfo(
@@ -18,6 +22,50 @@ public class InventoryInfo {
                     smelt.getFishingSpot().getId(),
                     smelt.getStatus(),
                     smelt.getType().getId()
+            );
+        }
+    }
+
+    public record DetailSmeltInfo(
+            SmeltInfo smeltInfo,
+            LetterInfo letterInfo,
+            CommentInfo commentInfo
+    ) {
+        public static DetailSmeltInfo from(Smelt smelt) {
+            return new DetailSmeltInfo(
+                    SmeltInfo.from(smelt),
+                    LetterInfo.from(smelt.getLetter()),
+                    CommentInfo.from(smelt.getLetter().getComment())
+            );
+        }
+    }
+
+    record LetterInfo(
+            Long id,
+            String senderName,
+            String title,
+            LocalDateTime createdTime
+    ) {
+        public static LetterInfo from(Letter letter) {
+            return new LetterInfo(
+                    letter.getId(),
+                    letter.getSenderName(),
+                    letter.getTitle(),
+                    letter.getCreatedTime()
+            );
+        }
+    }
+
+    record CommentInfo(
+            Long id,
+            String content,
+            LocalDateTime createdTime
+    ) {
+        public static CommentInfo from(Comment comment) {
+            return new CommentInfo(
+                    comment.getId(),
+                    comment.getContent(),
+                    comment.getCreatedTime()
             );
         }
     }
