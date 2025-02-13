d7b48ffc2bdad6ade6c0f5f4d2f7711b0c8e6a34
정현정 <wgwjh05169@gmail.com>
2025-02-13 17:54:38 +0900

    feat: FishingSpotInfo에 빙어의 자세한 정보를 담는 DTO 작성

--- src/main/java/my/fisherman/fisherman/fishingspot/application/dto/FishingSpotInfo.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/application/dto/FishingSpotInfo.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 
 import my.fisherman.fisherman.fishingspot.domain.FishingSpot;
+import my.fisherman.fisherman.smelt.domain.Question;
+import my.fisherman.fisherman.smelt.domain.Quiz;
 import my.fisherman.fisherman.smelt.domain.Smelt;
 
 public class FishingSpotInfo {
@@ -41,13 +43,60 @@ public class FishingSpotInfo {
         }
     }
 
-    public record SimpleSmelt (
+    public record SimpleSmelt(
         Long id,
         Long typeId,
         String status
     ) {
         public static SimpleSmelt from(Smelt smelt) {
             return new SimpleSmelt(smelt.getId(), smelt.getType().getId(), smelt.getStatus().name());
+        }
+    }
+
+    public record DetailSmelt(
+        Long id,
+        String senderName,
+        String title,
+        String content,
+        QuizInfo quiz
+    ) {
+        public static DetailSmelt of(Smelt smelt, List<Question> questions) {
+            return new DetailSmelt(
+                smelt.getId(), 
+                smelt.getLetter().getSenderName(),
+                smelt.getLetter().getTitle(),
+                smelt.getLetter().getContent(),
+                QuizInfo.of(smelt.getQuiz(), questions));
+        }
+    }
+
+    public record QuizInfo(
+        Long id,
+        String type,
+        String title,
+        List<QuestionInfo> questions
+    ) {
+        public static QuizInfo of(Quiz quiz, List<Question> questions) {
+            if (quiz == null || questions == null) {
+                return null;
+            }
+
+            return new QuizInfo(
+                quiz.getId(),
+                quiz.getType().name(),
+                quiz.getTitle(),
+                questions.stream().map(QuestionInfo::from).toList()
+            );
+        }
+    }
+
+    public record QuestionInfo(
+        Long id,
+        String content,
+        Boolean isAnswer
+    ) {
+        public static QuestionInfo from(Question question) {
+            return new QuestionInfo(question.getId(), question.getContent(), question.getIsAnswer());
         }
     }
 }
