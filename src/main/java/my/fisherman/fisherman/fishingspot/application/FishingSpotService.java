bdcda7b1582a1d0e2d8e22f6e268c4d014b080aa
정현정 <wgwjh05169@gmail.com>
2025-02-13 17:55:51 +0900

    feat: 빙어를 다른 낚시터에 보내는 기능 작성

--- src/main/java/my/fisherman/fisherman/fishingspot/application/FishingSpotService.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/application/FishingSpotService.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import my.fisherman.fisherman.fishingspot.application.dto.FishingSpotInfo;
 import my.fisherman.fisherman.fishingspot.domain.FishingSpot;
 import my.fisherman.fisherman.fishingspot.repository.FishingSpotRepository;
+import my.fisherman.fisherman.inventory.domain.Inventory;
+import my.fisherman.fisherman.inventory.repository.InventoryRepository;
+import my.fisherman.fisherman.security.util.SecurityUtil;
 import my.fisherman.fisherman.smelt.domain.Smelt;
 import my.fisherman.fisherman.smelt.repository.SmeltRepository;
 
@@ -20,6 +23,7 @@ public class FishingSpotService {
 
     private final FishingSpotRepository fishingSpotRepository;
     private final SmeltRepository smelRepository;
+    private final InventoryRepository inventoryRepository;
 
     public List<FishingSpotInfo.Simple> searchFishingSpot(String keyword) {
 
@@ -35,9 +39,24 @@ public class FishingSpotService {
         // TODO: NotFound 예외 처리
         FishingSpot fishingSpot = fishingSpotRepository.findById(fishingSpotId)
                                     .orElseThrow();
-
+                                    
         Page<Smelt> smeltPage = smelRepository.findAllByFishingSpot(fishingSpot, pageable);
-
+        
         return FishingSpotInfo.SmeltPage.of(fishingSpot, smeltPage);
+    }
+    
+    public FishingSpotInfo.DetailSmelt sendSmeltTo(Long fishingSpotId, Long smeltId) {
+        // TODO: 사용자 ID를 가져오지 못하는 예외 처리
+        Long userId = SecurityUtil.getCurrentUserId().orElseThrow();
+        
+        //TODO: NotFound 예외 처리
+        Inventory inventory = inventoryRepository.findByUser(userId).orElseThrow();
+        Smelt smelt = smelRepository.findById(smeltId).orElseThrow();
+        FishingSpot fishingSpot = fishingSpotRepository.findById(fishingSpotId).orElseThrow();
+
+        smelt.send(inventory, fishingSpot);
+
+        // TODO: 질문 조회
+        return FishingSpotInfo.DetailSmelt.of(smelt, null);
     }
 }
