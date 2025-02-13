241a1032e687cbb76b6f8f16715fc743a16d73d1
정현정 <wgwjh05169@gmail.com>
2025-02-13 17:53:59 +0900

    feat: InventoryRepository에 사용자로 조회하는 메소드 추가

--- src/main/java/my/fisherman/fisherman/inventory/repository/InventoryRepository.java
+++ src/main/java/my/fisherman/fisherman/inventory/repository/InventoryRepository.java
@@ -1,9 +1,13 @@
 package my.fisherman.fisherman.inventory.repository;
 
 import my.fisherman.fisherman.inventory.domain.Inventory;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByUser(Long userId);
 }
