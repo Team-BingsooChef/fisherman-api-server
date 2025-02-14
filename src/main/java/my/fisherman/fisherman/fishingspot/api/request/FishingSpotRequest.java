a20e642db1332e2e7b8a223aae1067390816572a
정현정 <wgwjh05169@gmail.com>
2025-02-14 17:10:44 +0900

    feat: FishingSpotRequest의 Send에 Command로 변환하는 메소드 작성

--- src/main/java/my/fisherman/fisherman/fishingspot/api/request/FishingSpotRequest.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/api/request/FishingSpotRequest.java
@@ -2,15 +2,33 @@ package my.fisherman.fisherman.fishingspot.api.request;
 
 import java.util.List;
 
+import my.fisherman.fisherman.fishingspot.application.command.SendSmeltCommand;
+
 public class FishingSpotRequest {
 
+    // TODO: 유효성 검증 추가
     public record Send(
             Long smeltId,
             String title,
             String content,
             String senderName,
             Quiz quiz
-    ) {}
+    ) {
+        public SendSmeltCommand toCommand(Long fishingSpotId) {
+            return new SendSmeltCommand(
+                fishingSpotId,
+                smeltId,
+                senderName,
+                title,
+                content,
+                true,
+                quiz.title,
+                quiz.content,
+                quiz.type,
+                quiz.questions,
+                quiz.answerIndex);
+        }
+    }
 
     record Quiz(
         String title,
