a14cccc034ce01086ffba91062707e8de51cf3dc
정현정 <wgwjh05169@gmail.com>
2025-02-14 17:07:10 +0900

    feat: QuestionRepository 인터페이스 작성

---
+++ src/main/java/my/fisherman/fisherman/smelt/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package my.fisherman.fisherman.smelt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository {
+}
