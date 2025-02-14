7bb3a5fddaad318d2839eba978ee01279dd66044
정현정 <wgwjh05169@gmail.com>
2025-02-14 17:24:40 +0900

    fix: JPA Repository에 클래스와 ID를 지정하는 파라미터 추가

--- src/main/java/my/fisherman/fisherman/smelt/repository/QuestionRepository.java
+++ src/main/java/my/fisherman/fisherman/smelt/repository/QuestionRepository.java
@@ -3,6 +3,8 @@ package my.fisherman.fisherman.smelt.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import my.fisherman.fisherman.smelt.domain.Question;
+
 @Repository
-public interface QuestionRepository extends JpaRepository {
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
