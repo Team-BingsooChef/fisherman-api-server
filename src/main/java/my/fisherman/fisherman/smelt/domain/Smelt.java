873c21e9bf10703272554d78aff38a1da9c13c74
정현정 <wgwjh05169@gmail.com>
2025-02-14 17:06:29 +0900

    feat: 빙어를 다른 낚시터에 보내는 로직 작성

--- src/main/java/my/fisherman/fisherman/smelt/domain/Smelt.java
+++ src/main/java/my/fisherman/fisherman/smelt/domain/Smelt.java
@@ -1,5 +1,6 @@
 package my.fisherman.fisherman.smelt.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,6 +17,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import my.fisherman.fisherman.fishingspot.domain.FishingSpot;
 import my.fisherman.fisherman.inventory.domain.Inventory;
+import my.fisherman.fisherman.user.domain.User;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -43,11 +45,11 @@ public class Smelt {
     private SmeltType type;
 
     @JoinColumn(name = "quiz_id")
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private Quiz quiz;
 
     @JoinColumn(name = "letter_id")
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private Letter letter;
 
     private Smelt(Inventory inventory, SmeltType type) {
@@ -63,20 +65,21 @@ public class Smelt {
         return new Smelt(inventory, type);
     }
 
-    public void send(Inventory inventory, FishingSpot fishingSpot) {
+    // user가 빙어를 fishingSpot으로 보낸다.
+    public void send(User user, FishingSpot fishingSpot, Letter letter, Quiz quiz) {
         // TODO: ID 비교로 수정
-        if (inventory != this.inventory) {
+        if (user != this.inventory.getUser()) {
             // TODO: 권한이 없는 경우 예외 던지기
             return;
         }
 
-        if (fishingSpot.getFisherman() == inventory.getUser()) {
+        if (user == fishingSpot.getFisherman()) {
             // TODO: 본인의 낚시터에 보내는 예외 던지기
             return;
         }
 
         this.fishingSpot = fishingSpot;
-
-        // TODO: 질문 등록
+        this.letter = letter;
+        this.quiz = quiz;
     }
 }
