ec202e2790c5448c6b032f80e55bed8a13ea0683
정현정 <wgwjh05169@gmail.com>
2025-02-18 00:20:28 +0900

    refactor: 사용하지 않는 메소드 선언 제거

--- src/main/java/my/fisherman/fisherman/inventory/repository/InventoryRepository.java
+++ src/main/java/my/fisherman/fisherman/inventory/repository/InventoryRepository.java
@@ -2,12 +2,9 @@ package my.fisherman.fisherman.inventory.repository;
 
 import my.fisherman.fisherman.inventory.domain.Inventory;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findByUser(Long userId);
 }
