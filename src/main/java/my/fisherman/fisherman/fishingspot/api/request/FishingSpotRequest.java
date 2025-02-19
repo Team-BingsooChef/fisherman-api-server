0afa71c358d897c45e83bf89211f60a3dc5fcafb
정현정 <wgwjh05169@gmail.com>
2025-02-19 11:29:33 +0900

    feat: 빙어 보내기 API의 제약 조건 작성

--- src/main/java/my/fisherman/fisherman/fishingspot/api/request/FishingSpotRequest.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/api/request/FishingSpotRequest.java
@@ -3,15 +3,27 @@ package my.fisherman.fisherman.fishingspot.api.request;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import my.fisherman.fisherman.fishingspot.application.command.FishingSpotCommand;
 
 public class FishingSpotRequest {
 
-    // TODO: 유효성 검증 추가
     public record Send(
+        @NotNull(message = "빙어 ID는 필수입니다.")
+        @Positive(message = "빙어 ID는 양수여야 합니다.")
         Long smeltId,
+        @NotBlank(message = "편지 제목은 필수입니다.")
+        @Length(max = 15, message = "편지의 제목은 15자 이하여야 합니다.")
         String title,
+        @NotBlank(message = "편지 내용은 필수입니다.")
+        @Length(max = 300, message = "편지의 내용은 300자 이하여야 합니다.")
         String content,
+        @NotBlank(message = "보낸이는 필수입니다.")
+        @Length(max = 300, message = "보낸이는 8자 이하여야 합니다.")
         String senderName,
         Optional<Quiz> quiz
     ) {
