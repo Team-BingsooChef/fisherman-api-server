834d2b2760ff31a1c62b4961f9cf0b43749f0775
정현정 <wgwjh05169@gmail.com>
2025-02-19 10:44:02 +0900

    feat: 핸들링 하지 않은 오류 발생 숨기기

--- src/main/java/my/fisherman/fisherman/global/exception/FishermanExceptionHandler.java
+++ src/main/java/my/fisherman/fisherman/global/exception/FishermanExceptionHandler.java
@@ -1,6 +1,8 @@
 package my.fisherman.fisherman.global.exception;
 
 import io.swagger.v3.oas.annotations.Hidden;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,4 +19,12 @@ public class FishermanExceptionHandler {
 
         return problemDetail;
     }
+
+    // @ExceptionHandler(RuntimeException.class)
+    // ProblemDetail handleRuntimeException(RuntimeException e) {
+    //     ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+    //     problemDetail.setTitle("서버에서 알 수 없는 오류가 발생했습니다.");
+
+    //     return problemDetail;
+    // }
 }
