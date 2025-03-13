637df8c75b7f23d2375e3e24d27e615bea54e3e8
sanghee0820 <sanghee0820.dev@gmail.com>
2025-03-13 10:25:23 +0900

    refactor: 에러 코드를 5글자로 변경

--- src/main/java/my/fisherman/fisherman/global/exception/code/UserErrorCode.java
+++ src/main/java/my/fisherman/fisherman/global/exception/code/UserErrorCode.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "U401", "사용자를 찾을 수 없습니다."),
-    ALREADY_EXISTS(HttpStatus.CONFLICT, "U402", "이미 존재하는 사용자입니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "U403", "권한이 없습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "US401", "사용자를 찾을 수 없습니다."),
+    ALREADY_EXISTS(HttpStatus.CONFLICT, "US402", "이미 존재하는 사용자입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "US403", "권한이 없습니다.");
 
     private HttpStatus httpStatus;
     private String code;
