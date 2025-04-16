669d2026a2bd653c85e00a824363eeb0c81fcb5f
정현정 <wgwjh05169@gmail.com>
2025-04-16 14:01:55 +0900

    refactor: Inventory를 엔티티가 아니라 ID로 조회하도록 수정

--- src/main/java/my/fisherman/fisherman/fishingspot/application/FishingSpotService.java
+++ src/main/java/my/fisherman/fisherman/fishingspot/application/FishingSpotService.java
@@ -24,12 +24,10 @@ import my.fisherman.fisherman.smelt.domain.Question;
 import my.fisherman.fisherman.smelt.domain.Quiz;
 import my.fisherman.fisherman.smelt.domain.QuizType;
 import my.fisherman.fisherman.smelt.domain.Smelt;
-import my.fisherman.fisherman.smelt.domain.SmeltType;
 import my.fisherman.fisherman.smelt.repository.LetterRepository;
 import my.fisherman.fisherman.smelt.repository.QuestionRepository;
 import my.fisherman.fisherman.smelt.repository.QuizRepository;
 import my.fisherman.fisherman.smelt.repository.SmeltRepository;
-import my.fisherman.fisherman.smelt.repository.SmeltTypeRepository;
 import my.fisherman.fisherman.user.domain.User;
 import my.fisherman.fisherman.user.repository.UserRepository;
 
@@ -41,7 +39,6 @@ public class FishingSpotService {
     private final FishingSpotRepository fishingSpotRepository;
     private final InventoryRepository inventoryRepository;
     private final SmeltRepository smeltRepository;
-    private final SmeltTypeRepository smeltTypeRepository;
     private final LetterRepository letterRepository;
     private final UserRepository userRepository;
     private final QuizRepository quizRepository;
@@ -86,18 +83,13 @@ public class FishingSpotService {
         Long userId = SecurityUtil.getCurrentUserId()
             .orElseThrow(() -> new FishermanException(UserErrorCode.FORBIDDEN));
 
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new FishermanException(FishingSpotErrorCode.NOT_FOUND, "현재 사용자를 찾을 수 없습니다."));
+        Inventory inventory = inventoryRepository.findByUserId(userId)
+                .orElseThrow(() -> new FishermanException(FishingSpotErrorCode.NOT_FOUND, "사용자 %d의 인벤토리를 찾을 수 없습니다.".formatted(userId)));
 
-        Inventory inventory = inventoryRepository.findByUser(user)
-                .orElseThrow(() -> new FishermanException(FishingSpotErrorCode.NOT_FOUND, "사용자 %d의 인벤토리를 찾을 수 없습니다.".formatted(user.getId())));
-        SmeltType smeltType = smeltTypeRepository.findById(command.smeltTypeId()).
-                orElseThrow(() -> new FishermanException(FishingSpotErrorCode.NOT_FOUND, "빙어 종류 %d을/를 찾을 수 없습니다.".formatted(command.smeltTypeId())));
-
-        Smelt smelt = smeltRepository.findDrewSmeltByInventoryAndType(inventory.getId(), smeltType.getId())
+        Smelt smelt = smeltRepository.findDrewSmeltByInventoryAndType(inventory.getId(), command.smeltTypeId())
                 .orElseThrow(() -> new FishermanException(
                     FishingSpotErrorCode.NOT_FOUND,
-                    "인벤토리 %d에서 빙어 종류가 %d인 보내지 않은 빙어를 찾을 수 없습니다.".formatted(inventory.getId(), smeltType.getId())));
+                    "인벤토리 %d에서 빙어 종류가 %d인 보내지 않은 빙어를 찾을 수 없습니다.".formatted(inventory.getId(), command.smeltTypeId())));
         FishingSpot fishingSpot = fishingSpotRepository.findById(command.fishingSpotId())
                 .orElseThrow(() -> new FishermanException(FishingSpotErrorCode.NOT_FOUND, "낚시터 %d을/를 찾을 수 없습니다.".formatted(command.fishingSpotId())));
 
@@ -123,7 +115,7 @@ public class FishingSpotService {
 
         Inventory senderInventory = smelt.getInventory();
         senderInventory.increaseCoin(5L);
-        Inventory recieverInventory = inventoryRepository.findByUser(smelt.getFishingSpot().getFisherman())
+        Inventory recieverInventory = inventoryRepository.findByUserId(smelt.getFishingSpot().getFisherman().getId())
                 .orElseThrow(() -> new FishermanException(FishingSpotErrorCode.NOT_FOUND, "낚시꾼의 인벤토리를 찾을 수 없습니다."));
         recieverInventory.increaseCoin(3L);
 
--- src/main/java/my/fisherman/fisherman/inventory/repository/InventoryRepository.java
+++ src/main/java/my/fisherman/fisherman/inventory/repository/InventoryRepository.java
@@ -2,7 +2,6 @@ package my.fisherman.fisherman.inventory.repository;
 
 import my.fisherman.fisherman.inventory.domain.Inventory;
 
-import my.fisherman.fisherman.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findByUser(User user);
+    Optional<Inventory> findByUserId(Long userId);
 }
